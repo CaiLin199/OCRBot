4c4cbcc9ec9e0ba615b442a04386e4059663b8a1
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-01-25 17:44:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,63 +2,13 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-#news
-RSS_URL = os.environ.get("RSS_URL", "https://myanimelist.net/rss/news.xml")
-#subsplease episode notifi
-RSS_URL2 = os.environ.get("RSS_URL2","https://subsplease.org/rss/?t&r=sd")
-CHECK_INTERVAL = int(os.environ.get("CHECK_INTERVAL", "60"))
-
-
-#Bot token @Botfather
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "0")
-
-#Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "26254064"))
-
-#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c")
-
-#Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002345564361"))
-
-CHANNEL_IDS = [-1002345564361, -1002315395252]  # Add your channel IDs here
-
-
-#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5296584067"))
-
-#Port
 PORT = os.environ.get("PORT", "8080")
-
-#Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://abidabdullahown10:abidabdullah1425@cluster0.h3iui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "ADMIN")
-
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002176591513"))
-
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "1"))
-
-#start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5296584067").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
-
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-
-ADMINS.append(OWNER_ID)
-ADMINS.append(5296584067)
-
 LOG_FILE_NAME = "filesharingbot.txt"
-
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -73,7 +23,5 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
