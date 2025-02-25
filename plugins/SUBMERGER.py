e809b631d2d072fed584d754b1023a1b27850469
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-26 03:37:21 +0600

    Update SUBMERGER.py

--- plugins/SUBMERGER.py
+++ plugins/SUBMERGER.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 @Bot.on_message(filters.user(OWNER_ID) & filters.command("logs"))
 async def get_log_file(client, message):
 	try:
-		await message.reply_document(document=LOG_FILE_NAME, caption=log file by SubMerger)
+		await message.reply_document(document=LOG_FILE_NAME, caption="log file by SubMerger")
 	except Exception as e:
 		logger.error(f"Failed to send log file to OWNER: {e}")
 		await message_reply(f"Error:{e}")
