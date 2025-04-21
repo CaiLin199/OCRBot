122b6ac2f600a22f8bb7e9ecef6a62da95b2784c
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-21 14:24:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "0")
 API_ID = int(os.environ.get("API_ID", "26254064"))
 API_HASH = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c")
 OWNER_ID = int(os.environ.get("OWNER_ID", "5296584067"))
-MAIN_CHANNEL = int(os.environ.get("MAIN_CHANNEL", "-1002234026927"))
-DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002279496397"))
+MAIN_CHANNEL = int(os.environ.get("MAIN_CHANNEL", "-1002372552947"))
+CHANNEL_ID = int(os.environ.get("DB_CHANNEL", "-1002279496397"))
 OWNER_IDS = [int(x) for x in os.environ.get("OWNER_IDS", "5296584067,5364178811").split(',')]
 THUMBNAIL = os.environ.get("THUMBNAIL", "Assist/thumbnail.jpg")
 STICKER_ID = os.environ.get("STICKER_ID", "CAACAgUAAxkBAAIJZGfLOdpxPmkKJ_nlJICh0bmi7GF1AALLFwACWARYVg4ubUgM9uuVNgQ")
@@ -15,8 +15,7 @@ PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "1"))
 
 #FORMATS
-POST_FORMAT = 
-"""
+POST_FORMAT = """
 {title}\n\n
 • EPISODE: {episode}
 • GENRAS: {genras}
