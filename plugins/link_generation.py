8f20aff94fb05c2f205c9338c147eb42da9a1e9e
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-22 14:06:45 +0600

    Update link_generation.py

--- plugins/link_generation.py
+++ plugins/link_generation.py
@@ -9,7 +9,7 @@ async def encode(string):
     base64_string = base64_bytes.decode('ascii')
     return base64_string
 
-async def generate_link(client, file_message):
+async def generate_link(client, file_message, user_data=None):  # Added optional user_data parameter
     """Generate shareable link for a file message."""
     try:
         msg_id = file_message.id
