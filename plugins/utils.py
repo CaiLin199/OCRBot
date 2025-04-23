a673d9d2f88b6b6f8f5e2685607adfc9b49f44e1
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-23 09:38:25 +0600

    Delete plugins/utils.py

--- plugins/utils.py
+++
@@ -1,9 +0,0 @@
-def humanbytes(size):
-    if not size:
-        return "0B"
-    units = ['B', 'KB', 'MB', 'GB', 'TB']
-    index = 0
-    while size >= 1024 and index < len(units) - 1:
-        size /= 1024
-        index += 1
-    return f"{size:.2f}{units[index]}"
