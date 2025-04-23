4d4a9a0ca651be9364f645722e4d07dcc6f9a167
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-23 11:40:11 +0600

    Update main_handler.py

--- plugins/main_handler.py
+++ plugins/main_handler.py
@@ -3,6 +3,7 @@
 from bot import Bot
 from config import OWNER_ID, CHANNEL_ID, MAIN_CHANNEL
 from pyrogram import filters
+from plugins.post_inputs import PostHandler
 
 @Bot.on_message(filters.private & filters.command("post") & filters.user(OWNER_ID))
     await post_handler.handle_post_command(client, message)
